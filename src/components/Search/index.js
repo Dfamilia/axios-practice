30cc3dcd48861c3592423f233bed1da360a8a06c
dfamilia <dfamilia7@gmail.com>
2020-02-10 10:14:33 -0400

    add functions to pokemonSearchList'

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Page from './page';
+import Axios from 'axios';
 
 
 export default class Search extends Component {
@@ -14,13 +15,42 @@ export default class Search extends Component {
         }
     }
 
+    componentDidMount(){
+        Axios.get('https://pokeapi.co/api/v2/pokemon?limit=964')
+        .then(data =>{
+            this.setState({ pokemonApiLoad: data.data.results})
+        })
+    }
+
     onChangeText = (event)=>{
-        console.log(event.target.name, event.target.value)
+        this.findPokemonIterations(event.target.value)
         this.setState({ text: event.target.value });
+        // console.log(event.target.name,this.state.pokemonApiLoad)
+
         
     }
 
+    findPokemonIterations = (text) => {
+        const pokemonReq = new RegExp(`^${text}`,'i');
+        const pokemonFounded = this.state.pokemonApiLoad.reduce((list, pokemon)=>
+        {
+            if(pokemonReq.test(pokemon.name)) list.push(pokemon.name)
+            return list;
+
+        }, []);
+
+        this.setState({pokemonSearchList: pokemonFounded});
+    }
+
     render() {
+
+        console.log("Api Load", this.state.pokemonApiLoad);
+
+        console.log("TEXT",this.state.text);
+
+        console.log("POKEMONS FOUNDED",this.state.pokemonSearchList)
+
+
         return (
             <Page onChangeText={this.onChangeText} text={this.state.text}/>
         )
